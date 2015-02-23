efa2a22e90b2e5a7a196289b5d2aeabd6ab9584b
Buck Shlegeris <bshlegeris@paypal.com>
2015-02-23 01:00:38 -0800

    better build tool

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 cp -r web-stuff/* ../bshlgrs.github.io/rpc/
 cp target/scala-2.11/rpc-9000-fastopt.js ../bshlgrs.github.io/target/scala-2.11/
+mv ../bshlgrs.github.io/rpc/index-dev.html ../bshlgrs.github.io/rpc/rpc.html
